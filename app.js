8d0ce80b01600e07421960a81d20aecb81ff90a5
jatin1213 <jatin@shyplite.com>
2023-04-10 00:34:20 +0530

    Just finish first lecture

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 let dummyData = [
-    {id: 'd1', name: 'Jatin', state: 'FGHJ', value: '10'},
-    {id: 'd2', name: 'Jatin1', state: 'RTYU', value: '12'},
-    {id: 'd3', name: 'Jatin2', state: 'CVBNM', value: '14'},
-    {id: 'd4', name: 'Jatin3', state: 'RTYGHB', value: '16'},
+    {id: 'd1', name: 'Jatin', state: 'FGHJ', value: '200'},
+    {id: 'd2', name: 'Jatin1', state: 'RTYU', value: '120'},
+    {id: 'd3', name: 'Jatin2', state: 'CVBNM', value: '140'},
+    {id: 'd4', name: 'Jatin3', state: 'RTYGHB', value: '160'},
 ]
 
-// d3.select('div')
-//     .selectAll('p')
-//     .data(dummyData)
-//     .enter()
-//     .append('p')
-//     .text( dta => dta.id);
+const xScale = d3.scaleBand().domain(dummyData.map( i => i.state)).rangeRound([0, 250]).padding(0.1);
+const yScale = d3.scaleLinear().domain([0, 250]).range([250, 0]);
 
+// scaleBand() > Uniform width of the array. 
+// rangeRound() > How much space is available. `Here we need to define min max value`
+// They not give height and width, they give a fucntion thats allow us to translate a value.
+// domain() > Which min and max value we want to be able to map into our chart.
+// range > [Mix, Min]
 
-const container = d3.select('div')
+const container = d3.select('svg')
     .classed('container', true) // Pushing the class in body
-    .style('border', '1px solid red');
+    .style('border', '1px solid green');
 
 const bar = container
     .selectAll('.bar')
     .data(dummyData) // Pushing dummy data in the bar section 
     .enter() // enter is check is present if not pushh the div or whatever you gonna push.
-    .append('div') // Append a div to body
+    .append('rect') // Append a div to body
     .classed('bar', true) // Pushing the class in body
-    .text();
+    .attr('width', xScale.bandwidth())
+    .attr('height', (aData) => 250 - yScale(aData.value))
+    .attr('x', (aData => xScale(aData.state)))
+    .attr('y', (aData => yScale(aData.value)))
+
+    ;
+
---
+++ garbage.js
@@ -0,0 +1,6 @@
+// d3.select('div')
+//     .selectAll('p')
+//     .data(dummyData)
+//     .enter()
+//     .append('p')
+//     .text( dta => dta.id);
